61978e7cfd86535ccefb1e9860b1374de796d9fb
dylan <dylancabral.93@gmail.com>
2022-09-21 16:36:37 -0700

    updating java for lab6

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let usersName = prompt('What is your name?'); 
+let usersNameGoesHere =('Hi ' + usersName + ' I appreciate you stopping by.');
+document.write('Hi ' + usersName + ' I appreciate you stopping by.');
+
+if(usersName.toLowerCase() == 'dylan'){
+alert('Wow what a cool name!')
+}
+else {
+alert('oh, well it would be a lot cooler if your name was dylan.')
+};
+
+confirm('You should only be here if you love heavy music')
+
